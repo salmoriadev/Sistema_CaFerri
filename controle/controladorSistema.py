90c12c3c3e047f1261e1f9e9499d0f3e12761343
arthur <salmoria.dev@gmail.com>
2025-09-24 11:06:08 -0300

    Adiciona a implementação parcial do controladorSistema e a main.

---
+++ controle/controladorSistema.py
@@ -0,0 +1,5 @@
+class ControladorSistema:   
+    def __init__(self):
+        pass
+    def inicializa_sistema(self):  
+        print("Sistema inicializado")
---
+++ main.py
@@ -0,0 +1,6 @@
+from controle.controladorSistema import ControladorSistema
+
+if __name__ == "__main__":
+  controlador = ControladorSistema()
+  controlador.inicializa_sistema()
+  
