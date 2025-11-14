b09efdd40b4cef14af634eb13a88fdb6346dfe7c
luigiferrim <luigiferrimaines@gmail.com>
2025-11-14 14:56:50 -0300

    Começando a fazer a tela estoque

--- limite/telaEstoque.py
+++ limite/telaEstoque.py
@@ -6,24 +6,54 @@
     usuário através do console, desacoplando a lógica de negócio do
     `ControladorEstoque` da entrada e saída de dados.
 """
-
+import FreeSimpleGUI as sg
 
 class TelaEstoque:
-    def tela_opcoes(self) -> int:
-        print("\n-------- Estoque ----------")
-        print("1 - Listar Inventário")
-        print("2 - Adicionar Novo Produto ao Estoque")
-        print("3 - Repor Estoque de um Produto")
-        print("4 - Dar Baixa Manual de um Produto")
-        print("0 - Retornar")
-        
-        try:
-            opcao = int(input("Escolha a opção: "))
-            return opcao
-        except ValueError:
-            self.mostra_mensagem("Entrada inválida! Digite um número.")
-            return None
 
+    def __init__(self):
+        self.__window = None
+
+    def init_opcoes(self):
+        sg.ChangeLookAndFeel('BluePurple') 
+        
+        opcoes_layout = [
+            [sg.Radio('Listar Inventário', "RD1", key='1', font='Any 12')],
+            [sg.Radio('Adicionar Novo Produto ao Estoque', "RD1", key='2', font='Any 12')],
+            [sg.Radio('Repor Estoque de um Produto', "RD1", key='3', font='Any 12')],
+            [sg.Radio('Dar Baixa Manual de um Produto', "RD1", key='4', font='Any 12')],
+            [sg.Radio('Retornar', "RD1", key='0', default=True, font='Any 12')]
+        ]
+
+        layout = [
+            [sg.Column([[sg.Text('-------- Estoque ---------', font=("Helvica", 25), pad=((0,0),(20,10)))]], justification='center')],
+            [sg.Column([[sg.Text('Escolha sua opção', font=("Helvica", 15), pad=((0,0),(0,20)))]], justification='center')],
+            [sg.Column([[sg.Frame('Opções de Estoque', opcoes_layout, font='Any 14', title_color='black')]], justification='center')],
+            [sg.Column([[
+                sg.Button('Confirmar', font='Any 12', pad=((10,10),(25,15))), 
+                sg.Cancel('Cancelar', font='Any 12', pad=((10,10),(25,15)))
+            ]], justification='center')]
+        ]
+        
+        self.__window = sg.Window('Gerenciador de Estoque', layout, element_justification='center', size=(550, 450))
+
+    def tela_opcoes(self) -> int:
+        self.init_opcoes() 
+        button, values = self.open()
+
+        if button in (None, 'Cancelar') or values['0']:
+            self.close()
+            return 0 
+
+        if button == 'Confirmar':
+            for i in range(1, 5):
+                key = str(i)
+                if values.get(key) == True:
+                    self.close()
+                    return i
+        
+        self.close()
+        return None 
+    
     def pega_dados_produto_estoque(self) -> dict:
         print("\n---- Dados do Produto no Estoque ----")
         try:
